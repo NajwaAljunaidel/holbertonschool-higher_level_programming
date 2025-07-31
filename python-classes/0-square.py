4aebdbf03b8518adc91caacb3a1f11b90a213f6a
Najwa <najwa.aljunaidel@gmail.com>
2025-07-31 14:49:11 +0300

    t0

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,13 +1,12 @@
 #!/usr/bin/python3
-
 """
-:write empty class Square that defines a square:
+write empty class Square that defines a square:
 
  """
 class Square:
 
     """ 
     An empty classes for now 
-    
+
     """
     pass
