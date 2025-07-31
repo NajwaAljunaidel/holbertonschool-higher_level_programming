19a4f9a137a3cc5caa6527ef8f675da8b7b9ffff
“NajwaAljunaidel” <najwa.aljunaidel@gmail.com>
2025-07-31 12:25:20 +0000

    0

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""empty class Square that defines a square."""
+'''empty class Square that defines a square.'''
 class Square:
-    """Represents an empty square."""
+    '''Represents an empty square.'''
     pass
