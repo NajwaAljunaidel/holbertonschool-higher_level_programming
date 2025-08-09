19666f477c7b097979f6e5a9016cd92f8134bbc6
“NajwaAljunaidel” <najwa.aljunaidel@gmail.com>
2025-08-09 18:01:21 +0000

    INHERTANCE WEEK

--- python-inheritance/0-lookup.py
+++ python-inheritance/0-lookup.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
+# 0-lookup.py
+# Brennan D Baraban <375@holbertonschool.com>
+"""Defines an object attribute lookup function."""
 
 
 def lookup(obj):
-    """returns all objects in an objects dictionary
-        -> as a list
-    """
-    return dir(obj)
+    """Return a list of an object's available attributes."""
+    return (dir(obj))
