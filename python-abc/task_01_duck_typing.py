cdd9181d31161c37be8095f022ae71c473e2bc0e
“NajwaAljunaidel” <najwa.aljunaidel@gmail.com>
2025-08-09 19:11:36 +0000

    p

--- python-abc/task_01_duck_typing.py
+++ python-abc/task_01_duck_typing.py
@@ -1,41 +1,59 @@
-# task_01_duck_typing.py
-
+#!/usr/bin/python3
+"""
+This contains code to manipulate shapes
+"""
 from abc import ABC, abstractmethod
-import math
+from math import pi
+
 
 class Shape(ABC):
+    """
+    represent a shape
+    """
+
     @abstractmethod
-    def area(self):
-        """Calculer l'aire de la forme."""
+    def area(self) -> float:
         pass
 
     @abstractmethod
-    def perimeter(self):
-        """Calculer le périmètre de la forme."""
+    def perimeter(self) -> float:
         pass
+
 
 class Circle(Shape):
-    def __init__(self, radius):
-        self.radius = radius
+    """
+    represent a circle
+    """
 
-    def area(self):
-        return math.pi * self.radius ** 2
+    def __init__(self, radius) -> None:
+        self.radius = abs(radius)
 
-    def perimeter(self):
-        return 2 * math.pi * self.radius
+    def area(self) -> float:
+        return pi * self.radius**2
+
+    def perimeter(self) -> float:
+        return 2 * pi * self.radius
+
 
 class Rectangle(Shape):
-    def __init__(self, width, height):
-        self.width = width
-        self.height = height
+    """
+    represent a rectangle
+    """
 
-    def area(self):
-        return self.width * self.height
+    def __init__(self, width, height) -> None:
+        self.__width = width
+        self.__height = height
 
-    def perimeter(self):
-        return 2 * (self.width + self.height)
+    def area(self) -> float:
+        return self.__width * self.__height
 
-def shape_info(shape):
-    """Affiche l'aire et le périmètre de la forme donnée."""
-    print(f"Area: {shape.area()}")
-    print(f"Perimeter: {shape.perimeter()}")
+    def perimeter(self) -> float:
+        return 2 * self.__height + 2 * self.__width
+
+
+def shape_info(obj) -> None:
+    """
+    print shape obj info
+    """
+    print(f"Area: {obj.area()}")
+    print(f"Perimeter: {obj.perimeter()}")
