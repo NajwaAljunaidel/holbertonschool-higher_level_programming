471da13c8578b4f82c442e3a9840e8486187d85f
“NajwaAljunaidel” <najwa.aljunaidel@gmail.com>
2026-01-06 07:14:15 +0000

    solve the debugg

--- python-import_modules/4-hidden_discovery.py
+++ python-import_modules/4-hidden_discovery.py
@@ -1,7 +1,12 @@
 #!/usr/bin/python3
-import hidden_4
+# 4-hidden_discovery.py
+# Brennan D Baraban <375@holbertonschool.com>
 
 if __name__ == "__main__":
-    for name in dir(hidden_4):
-        if not name.startswith("__"):
+    """Print all names defined by hidden_4 module."""
+    import hidden_4
+
+    names = dir(hidden_4)
+    for name in names:
+        if name[:2] != "__":
             print(name)
